c957479cf643ff1c364bc74bf5f825244c65a1b4
Rafael Zabotto <rafaelm.zabotto@gmail.com>
2018-11-12 20:51:09 -0200

    Database Funcional

--- ProjetoLP3/src/Model/database/conexao.java
+++ ProjetoLP3/src/Model/database/conexao.java
@@ -7,13 +7,9 @@ import java.sql.Statement;
 
 public class conexao {
 
-//    private final String BD = "jdbc:mysql://localhost/instituicao";
-//    private final String USER = "root";
-//    private final String PASSWORD = "";
-
     private final String BD = "jdbc:mysql://localhost/instituicao";
-    private final String USER = "root";
-    private final String PASSWORD = "password";
+    private final String USER = "";
+    private final String PASSWORD = "";
 
 
 
